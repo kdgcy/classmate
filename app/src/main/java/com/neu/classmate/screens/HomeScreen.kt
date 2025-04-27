fb47081279d289c2befe9dcef3856da0996d9cb8
Kenneth D. <dagacayken@gmail.com>
2025-04-27 09:46:56 +0800

    add new file and edit headerview for HomeScree

--- app/src/main/java/com/neu/classmate/screens/HomeScreen.kt
+++ app/src/main/java/com/neu/classmate/screens/HomeScreen.kt
@@ -1,8 +1,9 @@
 package com.neu.classmate.screens
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -13,27 +14,29 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import com.neu.classmate.components.HeaderView
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier, navController: NavHostController){
+fun HomeScreen(modifier: Modifier = Modifier, navController: NavHostController) {
+
+
     Column(
         modifier = Modifier.fillMaxSize().padding(32.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Welcome to home screen"
-        )
-
-        Button(onClick = {
-            Firebase.auth.signOut()
-            navController.navigate(Routes.AuthScreen){
-                popUpTo(Routes.HomeScreen){inclusive=true}
-            }
-        }) {
-            Text(
-                text = "Logout"
-            )
+    ) {
+        HeaderView()
+        // Logout Button
+        Button(
+            onClick = {
+                Firebase.auth.signOut()
+                navController.navigate(Routes.AuthScreen) {
+                    popUpTo(Routes.HomeScreen) { inclusive = true }
+                }
+            },
+            modifier = Modifier.fillMaxWidth().height(65.dp)
+        ) {
+            Text("Logout")
         }
-    }
-}
+
+    }//Column End
+}//HomeScreen END
+
