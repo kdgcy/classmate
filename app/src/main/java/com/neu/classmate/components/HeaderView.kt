37fec22a7506430781ddd35fc2c8729e51ddcb33
Kenneth D. <dagacayken@gmail.com>
2025-04-28 10:27:04 +0800

    resize the default icon for user

--- app/src/main/java/com/neu/classmate/components/HeaderView.kt
+++ app/src/main/java/com/neu/classmate/components/HeaderView.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -16,8 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -61,6 +66,9 @@ fun HeaderView(modifier: Modifier = Modifier){
             )
         }
 
-        Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+        Icon(
+            imageVector = Icons.Default.AccountCircle, contentDescription = null,
+            modifier = Modifier.size(32.dp)
+        )
     }
 }
