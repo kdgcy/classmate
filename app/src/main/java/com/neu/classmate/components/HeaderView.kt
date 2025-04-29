cc232e14a5b7e7eb114bb07da3f2281df007f938
Kenneth D. <dagacayken@gmail.com>
2025-04-29 10:57:36 +0800

    add routes to add task and implement title and calendar system

--- app/src/main/java/com/neu/classmate/components/HeaderView.kt
+++ app/src/main/java/com/neu/classmate/components/HeaderView.kt
@@ -1,16 +1,34 @@
 package com.neu.classmate.components
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Done
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -75,13 +93,32 @@ fun HeaderView(modifier: Modifier = Modifier, navController: NavHostController) 
                 onDismissRequest = { expanded = false }
             ) {
                 DropdownMenuItem(
-                    text = { Text("Profile") },
+                    leadingIcon = {
+                        Icon(Icons.Rounded.Add,
+                            contentDescription = null)
+                                  },
+                    text = { Text("Add Task") },
                     onClick = {
                         expanded = false
-                        // TODO: Navigate to profile screen here
+                        navController.navigate(Routes.TaskScreen)
                     }
                 )
                 DropdownMenuItem(
+                    leadingIcon = {
+                        Icon(Icons.Rounded.AccountCircle,
+                            contentDescription = null)
+                    },
+                    text = { Text("Profile") },
+                    onClick = {
+                        expanded = false
+                        //implement code here...
+                    }
+                )
+                DropdownMenuItem(
+                    leadingIcon = {
+                        Icon(Icons.Rounded.Done,
+                            contentDescription = null)
+                    },
                     text = { Text("Logout") },
                     onClick = {
                         expanded = false
@@ -91,7 +128,7 @@ fun HeaderView(modifier: Modifier = Modifier, navController: NavHostController) 
                         }
                     }
                 )
-            }
-        }
-    }
-}
+            }//Dropdown END
+        }//Box END
+    }//Row END
+}//Fun END
